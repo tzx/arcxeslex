c0924a7bc36ac71d69bb471b76cc7b6219238d50
Timmy Xiao <timmyxiao1@gmail.com>
2021-06-08 19:31:15 -0400

    Token datatype

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,50 @@
 module Main where
 
+data Token = TkIf               -- Keywords begin
+            | TkBool
+            | TkBreak
+            | TkImport
+            | TkContinue
+            | TkElse
+            | TkFalse
+            | TkFor
+            | TkWhile
+            | TkInt
+            | TkReturn
+            | TkLen
+            | TkTrue
+            | TkVoid            -- Keywords end
+            | TkStrLiteral String
+            | TkCharLiteral String
+            | TkIntLiteral String
+            | TkHexLiteral String
+            | TkIdentifier String
+            | TkSemicolon       -- Special symbols
+            | TkComma
+            | TkOpenBracket     -- Bracket: [], Braces: {}, Paren: ()
+            | TkOpenBraces
+            | TkOpenParen
+            | TkCloseBracket
+            | TkCloseBraces
+            | TkCloseParen
+            | TkPlus
+            | TkMinus
+            | TkExclamation
+            | TkAsterisk
+            | TkFSlash
+            | TkPercent
+            | TkEqual
+            | TkLess
+            | TkGreater
+            | TkLessEqual
+            | TkGreaterEqual
+            | TkEqualEqual
+            | TkNotEqual
+            | TkPlusEqual
+            | TkMinusEqual
+            | TkAnd
+            | TkOr
+            deriving (Eq, Show)
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
